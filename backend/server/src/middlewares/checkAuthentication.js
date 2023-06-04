ce488f9ab290e8c03a958b7b32aed024d51622f5
sofiaCoder <sofia.laas@gmail.com>
2023-06-04 15:16:04 +0200

    Authentication middleware added

---
+++ backend/server/src/middlewares/checkAuthentication.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+exports.checkAuthentication = function checkAuthentication(req, res, next) {
+    const {authKey} = req.cookies
+
+    if (!authKey) {
+        res.status(401).send('Missing authentication token')
+        return;
+    }
+    try {
+        const authKeyDecoded = jwt.verify(authKey, process.env.JWT_SECRET)
+        const { id } = authKeyDecoded
+        req.id = id
+        res.status(200)
+        next()
+    }
+    catch(error) {
+        res.status(401).send('Not authorized, wrong token' + error);
+    }
+}
