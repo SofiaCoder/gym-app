00f1080eace2ce29d0152311465db3529420b740
sofiaCoder <sofia.laas@gmail.com>
2023-06-13 15:20:22 +0200

    server updated

--- backend/server/src/server.js
+++ backend/server/src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const { authRouter } = require('./routes/authentication');
+const { friendRouter } = require('./routes/friendRouter');
 const server = express();
+const cookieParser = require('cookie-parser');
+const { usersRouter } = require('./routes/usersRouter');
 server.use(express.json());
+server.use(cookieParser());
 
 server.use('/auth', authRouter);
+server.use('/friends', friendRouter);
+server.use('/users', usersRouter);
 
 exports.server = server;
