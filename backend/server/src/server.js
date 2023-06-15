8c821f443b77db3e4b350f9fbd00121a76a40e0a
sofiaCoder <sofia.laas@gmail.com>
2023-06-15 12:01:37 +0200

    Cors extension added

--- backend/server/src/server.js
+++ backend/server/src/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { authRouter } = require('./routes/authentication');
-const { friendRouter } = require('./routes/friendRouter');
 const server = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { usersRouter } = require('./routes/usersRouter');
+const { authRouter } = require('./routes/authentication');
+const { friendRouter } = require('./routes/friendRouter');
 server.use(express.json());
 server.use(cookieParser());
+server.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 
 server.use('/auth', authRouter);
 server.use('/friends', friendRouter);
