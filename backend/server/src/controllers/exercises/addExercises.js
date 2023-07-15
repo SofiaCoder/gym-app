8c40c5a85e3dd6dc4f07989bbbfc293ea1d7d43c
sofiaCoder <sofia.laas@gmail.com>
2023-07-15 12:35:10 +0200

    Get and add exercise added in frontend

---
+++ backend/server/src/controllers/exercises/addExercises.js
@@ -0,0 +1,44 @@
+const main = require('../../database');
+const joi = require('joi');
+
+const validMuscleGroups = [
+  'shoulders',
+  'chest',
+  'arms',
+  'abs',
+  'back',
+  'legs',
+  'cardio',
+];
+
+const schema = joi.object({
+  exerciseName: joi.string().min(1).required(),
+  muscleGroup: joi.string().valid(...validMuscleGroups).required(),
+});
+
+exports.addExercise = async function addExercise(req, res) {
+  try {
+    const { exerciseCollection } = await main();
+    const { exerciseName, muscleGroup } = req.body;
+
+    const exerciseBody = {
+      exerciseName: exerciseName.toLowerCase(),
+      muscleGroup: muscleGroup.toLowerCase(),
+    };
+
+    const { error } = schema.validate(exerciseBody);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+
+    const existingExercise = await exerciseCollection.findOne({ exerciseName: exerciseBody.exerciseName });
+    if (existingExercise) {
+      return res.status(400).send('Exercise already exists in the database');
+    }
+
+    await exerciseCollection.insertOne(exerciseBody);
+    res.status(201).send(`${exerciseName} added`);
+  } catch (err) {
+    res.status(500).send(`Internal server error - ${err}`);
+  }
+};
---
+++ backend/server/src/controllers/exercises/getAllExercises.js
@@ -0,0 +1,18 @@
+const main = require('../../database');
+
+exports.getAllExercises = async function getAllExercises(req, res) {
+  try {
+    const { exerciseCollection } = await main();
+
+    const exercises = await exerciseCollection
+      .find({}, { projection: { _id: 0, exerciseName: 1, muscleGroup: 1 } })
+      .toArray();
+    if (exercises.length === 0) {
+      res.status(404).send('There is no exercises');
+    } else {
+      res.status(200).json(exercises);
+    }
+  } catch (err) {
+    res.status(500).send(`Internal server error - ${err}`);
+  }
+};
