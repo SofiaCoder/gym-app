93bd5670733ff5481db76cf851acf7b5c73688c1
sofiaCoder <sofia.laas@gmail.com>
2023-07-17 22:12:20 +0200

    router updated with patch and delete

--- backend/server/src/routes/exerciseRouter.js
+++ backend/server/src/routes/exerciseRouter.js
@@ -1,8 +1,12 @@
 const exerciseRouter = require('express').Router();
 const { addExercise } = require('../controllers/exercises/addExercises');
+const { deleteExercise } = require('../controllers/exercises/deleteExercise');
+const { editExercise } = require('../controllers/exercises/editExercise');
 const { getAllExercises } = require('../controllers/exercises/getAllExercises');
 
 exerciseRouter.get('/', getAllExercises);
 exerciseRouter.post('/', addExercise);
+exerciseRouter.patch('/', editExercise);
+exerciseRouter.delete('/', deleteExercise);
 
 exports.exerciseRouter = exerciseRouter;
