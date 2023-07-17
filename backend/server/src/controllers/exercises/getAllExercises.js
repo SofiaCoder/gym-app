7161dda13b8692134b8807f416b30bbb689d4572
sofiaCoder <sofia.laas@gmail.com>
2023-07-17 22:11:54 +0200

    id added in find

--- backend/server/src/controllers/exercises/getAllExercises.js
+++ backend/server/src/controllers/exercises/getAllExercises.js
@@ -5,7 +5,7 @@ exports.getAllExercises = async function getAllExercises(req, res) {
     const { exerciseCollection } = await main();
 
     const exercises = await exerciseCollection
-      .find({}, { projection: { _id: 0, exerciseName: 1, muscleGroup: 1, exerciseDescription: 1 } })
+      .find({}, { projection: { exerciseName: 1, muscleGroup: 1, exerciseDescription: 1 } })
       .toArray();
     if (exercises.length === 0) {
       res.status(404).send('There is no exercises');
