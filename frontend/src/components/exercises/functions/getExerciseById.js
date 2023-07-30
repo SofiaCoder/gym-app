20a039902e327a39b91e96a6e3c772c985f05322
sofiaCoder <sofia.laas@gmail.com>
2023-07-30 10:04:57 +0200

    Correct endpoint

--- frontend/src/components/exercises/functions/getExerciseById.js
+++ frontend/src/components/exercises/functions/getExerciseById.js
@@ -1,5 +1,5 @@
 const getExerciseById = async (exerciseId) => {
-  const res = await fetch(`http://localhost:8080/exercises/${exerciseId}`, {
+  const res = await fetch(`http://localhost:8080/exercises/byId/${exerciseId}`, {
     credentials: 'include',
   });
   if (res.status === 401) {
