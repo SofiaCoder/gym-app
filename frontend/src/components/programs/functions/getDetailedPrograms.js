6f1133e6d376995ac13d7df8f45c71ef6017f47c
sofiaCoder <sofia.laas@gmail.com>
2023-08-06 21:02:57 +0200

    BugFix - send empty array instead of res.status

--- frontend/src/components/programs/functions/getDetailedPrograms.js
+++ frontend/src/components/programs/functions/getDetailedPrograms.js
@@ -2,11 +2,11 @@ const getDetailedPrograms = async (programID) => {
   const res = await fetch(`http://localhost:8080/programs/detailed/${programID}`, {
     credentials: 'include',
   });
-  if (res.status === 401) {
-    return res.status;
+  if (res.status === 404) {
+    return []
   }
-  const data = await res.json();
-  return data;
+
+  return await res.json();
 };
 
 export { getDetailedPrograms };
