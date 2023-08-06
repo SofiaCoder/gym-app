3db4348ed14e1235ade87d3ca18b889d947b31c4
sofiaCoder <sofia.laas@gmail.com>
2023-08-06 16:23:30 +0200

    Changed form in exercisePage (LandingPage)

--- frontend/src/pages/ExercisePage.jsx
+++ frontend/src/pages/ExercisePage.jsx
@@ -3,17 +3,23 @@ import { ExerciseForm } from '../components/Form/ExerciseForm';
 import './scss/ExercisePage.scss';
 import { postExercise } from '../components/exercises/functions/postExercise.js';
 import { FriendPage } from './FriendPage';
+import { ProgramForm } from '../components/Form/ProgramForm';
+import { postProgram } from '../components/programs/functions/postProgram';
 
 const ExercisePage = () => {
   const user = localStorage.getItem('username');
   return (
     <div className='ExercisePage'>
       <h1>{user}'s training</h1>
-      <ExerciseForm
+      <ProgramForm 
+        className='exerciseForm'
+        submitFunction={postProgram}
+        btnText='Add' />
+      {/* <ExerciseForm
         className='exerciseForm'
         submitFunction={postExercise}
         btnText='Add'
-      />
+      /> */}
       <Exercises className='Exercises' />
       <FriendPage className='friendPage' />
     </div>
