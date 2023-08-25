9a92315984b6372d1ea84953d3e18c5c89dbdf30
sofiaCoder <sofia.laas@gmail.com>
2023-08-25 08:32:43 +0200

    Endpoints added to router

--- backend/server/src/routes/programRouter.js
+++ backend/server/src/routes/programRouter.js
@@ -1,6 +1,9 @@
 const programRouter = require('express').Router();
 const { addExercisesToProgram } = require('../controllers/programs/addExercisesToProgram');
 const { createPrograms } = require('../controllers/programs/createPrograms');
+const { deleteExerciseFromProgram } = require('../controllers/programs/deleteExerciseFromProgram');
+const { deleteProgram } = require('../controllers/programs/deletePrograms');
+const { editProgram } = require('../controllers/programs/editProgram');
 const { getDetailedPrograms } = require('../controllers/programs/getDetailedProgram');
 const { getUsersPrograms } = require('../controllers/programs/getUsersPrograms');
 const { checkAuthentication } = require('../middleware/middleware');
@@ -10,6 +13,9 @@ programRouter.use(checkAuthentication);
 programRouter.get('/', getUsersPrograms);
 programRouter.get('/detailed/:programID', getDetailedPrograms);
 programRouter.post('/', createPrograms);
-programRouter.patch('/', addExercisesToProgram);
+programRouter.patch('/exercises', addExercisesToProgram);
+programRouter.patch('/', editProgram);
+programRouter.delete('/', deleteProgram);
+programRouter.delete('/exercise', deleteExerciseFromProgram);
 
 exports.programRouter = programRouter;
