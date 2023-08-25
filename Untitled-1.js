884f29a2783370dba078c976dd4f04127a5281d1
sofiaCoder <sofia.laas@gmail.com>
2023-08-25 08:30:46 +0200

    Saved inactive function from frontend

---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+
+
+
+    // let exerciseInfo = [];
+    // const exercisesInProgram = usersPrograms.map((program) => program.exercises)
+    // const exerciseIds = exercisesInProgram.flat().map((exercises) => exercises.exercise_id);
+    // for (const exercise of exerciseIds){
+    //   const fetchedExercise = await getExerciseById(exercise)
+    //   exerciseInfo.push(fetchedExercise)
+    // }
+    // exerciseInfo = exerciseInfo.flat()
+    // const flattenedExerciseInProgram = exercisesInProgram.flat()
+
+    // const detailedPrograms = flattenedExerciseInProgram.map((exercise1) => {
+    //   const matchingExercises = exerciseInfo.find((exercise2) => exercise1.exercise_id === exercise2._id) 
+    //     if (matchingExercises){
+    //       return{ ...exercise1, ...matchingExercises}
+    //     }
+    //     return exercise1
+    // });
+
+    // setDetailedPrograms(detailedPrograms);
